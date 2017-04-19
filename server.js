a6003a8b0fd6af31242deeb6fa095887805586b2
Yoshihiro Luk <hiroluk@gmail.com>
2017-04-19 01:52:40 -0700

    Pseudo coded list of items

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
-console.log("Running...");
-
 var crypto = require('crypto');
 var request = require('request');
 
 request('https://poloniex.com/public?command=returnTicker', function (error, response, body) {
     if (!error && response.statusCode == 200) {
 
-        console.log(body) // Print the google web page.
+        console.log(body);
      }
-})
+});
+
+// create curl request url for both private and public apis
+
+// create signature for private request with crypto and hmacsha signature
+
+// create class for access to object or follow poloniex api method for creating formulas
+
+// create dashboard to convert all traded coins to dollar amount
+
+// create script to convert fees to get profit or loss amount
+
+// 
