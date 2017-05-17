6a5b9f4063efa6451324a0d7c2291cf97dd24bbd
Yoshi <hiroluk@gmail.com>
2017-05-17 00:19:33 -0700

    Added Poloniex.js library, created Poloniex-wrapper file

--- server.js
+++ server.js
@@ -1,22 +1,9 @@
-var Poloniex = require('./lib/poloniex-wrapper.js');
-var Test = require('./lib/test.js');
-
-var t = Test.Poloniex;
-
-// console.log(t()._getPrivateHeaders()); // gets "test"
-
-var p = new t();
-console.log(p.testFunction()); // In Prototype
+require('dotenv').config();
+var poloniex = require('./lib/poloniex-wrapper.js');
 
 
 return;
 
-
-// create curl request url for both private and public apis
-
-// create signature for private request with crypto and hmacsha signature
-
-// create class for access to object or follow poloniex api method for creating formulas
 
 // create dashboard to convert all traded coins to dollar amount
 
