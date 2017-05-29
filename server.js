b93821711b53b65a4b5c9c247c02e0afb1c7a77c
Yoshihiro Luk <hiroluk@gmail.com>
2017-05-28 22:41:06 -0700

    Cleaned poloniex js

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ var Poloniex = require('./lib/poloniex-wrapper.js');
 var express = require('express');
 var app = express();
 
-
 app.get('/', function(req, res) {
   res.send('Hello World!');
 });
@@ -12,11 +11,3 @@ app.get('/', function(req, res) {
 app.listen(3000, function() {
   console.log('Example app listening on port 3000!');
 });
-
-// Poloniex.returnTicker(function(res, err) {
-// 	console.log(res);
-// 	console.log(err);
-// });
-// create dashboard to convert all traded coins to dollar amount
-
-// create script to convert fees to get profit or loss amount
