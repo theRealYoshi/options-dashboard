918cb4187c711621302d4505f100c6ffa6b6c4c5
Yoshihiro Luk <hiroluk@gmail.com>
2017-05-29 14:39:51 -0700

    Added mongodb database

--- app/routes.js
+++ app/routes.js
@@ -6,7 +6,6 @@ module.exports = function(app, passport) {
     // HOME PAGE (with login links) ========
     // =====================================
     app.get('/', function(req, res) {
-
         res.render('index.ejs'); // load the index.ejs file
     });
 
---
+++ config/database.js
@@ -0,0 +1,3 @@
+module.exports = {
+	'url' : process.env.MLAB_DB_URL || 'mongodb://' + process.env.DATABASE_DEV_USERNAME + ':' + process.env.DATABASE_DEV_PASSWORD + '@ds155841.mlab.com:55841/yoshi_options_dev'
+}
