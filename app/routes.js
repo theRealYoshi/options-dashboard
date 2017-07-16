e82d64d97f64ac415989e211ab20627c17a5f4c9
Yoshihiro Luk <hiroluk@gmail.com>
2017-07-16 11:20:13 -0700

    Added current open orders, pared down current margin positions, changed html to remove exchange balances

--- app/routes.js
+++ app/routes.js
@@ -23,10 +23,12 @@ module.exports = function(app, passport) {
         });
 
         balances.then(function(data) {
-            var accountBalance = {};
             var usdCurrencies = {};
-            var marginAccount = {};
-            var currentMargin = {};
+            var btcDollarValue = null;
+            var marginAccount = { 'btc' : {},
+                                  'dollar' : {} };
+            var currentMarginPositions = {};
+            var currentOpenOrders = {};
 
             Object.keys(data).forEach(function(key) {
                 var accountObj = data[key];
@@ -34,37 +36,64 @@ module.exports = function(app, passport) {
                 switch(accountKey) {
                     case 'usdCurrencies':
                         usdCurrencies = accountObj[accountKey];
+                        btcDollarValue = usdCurrencies['BTC']['last'];
                         break;
                     case 'marginAccountSummary':
-                        marginAccount = accountObj[accountKey];
+                        var marginBtcAccount = accountObj[accountKey];
+                        Object.keys(marginBtcAccount).forEach(function(key) {
+                            if (key === 'currentMargin') { return; }
+                            var value = marginBtcAccount[key];
+                            marginAccount['btc'][key] = value
+                            value = value * btcDollarValue;
+                            marginAccount['dollar'][key] = value.toFixed(2);
+                        });
                         break;
-                    case 'completeBalances':
-                        accountBalance = accountObj[accountKey];
+                    case 'currentMarginPositions':
+                        currentMarginPositionsObj = accountObj[accountKey];
+                        Object.keys(currentMarginPositionsObj).forEach(function(key) {
+                            var marginPositionObj = currentMarginPositionsObj[key];
+                            if (marginPositionObj.type === 'none' &&
+                                !marginPositionObj.amount <= 0) {
+                                return;
+                            }
+                            currentMarginPositions[key] = marginPositionObj;
+                            // convert to dollar
+                        });
                         break;
-                    case 'marginPositions':
-                        marginPositions = accountObj[accountKey];
-                        break;
+                    case 'currentOpenOrders':
+                        currentOpenOrdersObj = accountObj[accountKey];
+                        Object.keys(currentOpenOrdersObj).forEach(function(key) {
+                            var currencyPairOpenOrders = currentOpenOrdersObj[key];
+                            if (currencyPairOpenOrders.length <= 0) { return; }
+                            currentOpenOrders[key] = [];
+
+                            // Each open order within currency pair
+                            for (var i = 0; i < currencyPairOpenOrders.length; i++) {
+                                var openOrderObj = currencyPairOpenOrders[i];
+                                if (!openOrderObj.orderNumber &&
+                                    !openOrderObj.amount ) {
+                                    continue;
+                                }
+                                var orderNumber = openOrderObj.orderNumber;
+                                currentOpenOrders[key][orderNumber] = openOrderObj;
+                            }
+
+                            //convert to dollar
+                            // projected profit
+                        });
                     default:
                         break;
                 }
             });
 
-            var btcDollarValue = usdCurrencies['BTC']['last'];
-
-            Object.keys(marginAccount).forEach(function(key) {
-                if (key === 'currentMargin') { return; }
-                var value = marginAccount[key];
-                value = value * btcDollarValue;
-                marginAccount[key] = value.toFixed(2);
-            });
-
             res.render('dashboard.ejs', {
-                accountBalance : accountBalance,
-                marginAccountSummary : marginAccount,
-                marginPositions : marginPositions
+                marginAccount : marginAccount,
+                currentMarginPositions : currentMarginPositions,
+                currentOpenOrders : currentOpenOrders
             });
         })
         .catch(function(error) {
+            console.log(error);
             res.render('error.ejs');
         })
 
